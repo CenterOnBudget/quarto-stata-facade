eb0c2db6534a73986ef39e8f65666f81dfe0d3da
cz <41382462+c-zippel@users.noreply.github.com>
2023-03-30 17:47:30 -0400

    Simplify thanks to fix in Quarto 1.3
    As of version 1.3, Quarto consolidates output split across multiple cells (https://github.com/quarto-dev/quarto-cli/pull/5042), so this extension no longer needs to attempt to do so. stata-facade now requires at least Quarto version 1.3

--- README.md
+++ README.md
@@ -6,26 +6,30 @@ A Quarto extension that hides the evidence of faking Stata dynamic content with 
 
 To install this extension in your current directory (or into the Quarto project that you're currently working in), use the following command:
 
-```
-quarto install extension centeronbudget/quarto-stata-facade
-```
+    quarto add centeronbudget/quarto-stata-facade
 
-This will install the extension under the _extensions subdirectory. If you're using version control, you will want to check in this directory.
+This will install the extension under the \_extensions subdirectory. If you're using version control, you will want to check in this directory.
+
+stata-facade requires Quarto version 1.3 or higher.
 
 ## Usage
 
 To enable the extension, add stata-facade to the list of filters for your document or project:
 
-```
----
-title: My Document
-format: html
-filters:
-  - stata-facade
----
-```
+    ---
+    title: My Document
+    format: html
+    filters:
+      - stata-facade
+    ---
 
 All executable Python code blocks which use Stata cell magic will automatically be outputted as Stata code blocks, with appropriate syntax highlighting and stripped of the Stata cell magic command.
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](https://github.com/centeronbudget/quarto-stata-facade/blob/main/example.qmd)
+
+Here is the [output of example.qmd](https://centeronbudget.github.io/quarto-stata-facade).
 
 ## Warning
 
--- _extensions/stata-facade/stata-facade.lua
+++ _extensions/stata-facade/stata-facade.lua
@@ -12,67 +12,9 @@ CodeBlock = function(block)
   return block
 end
 
--- Transform ast of cells where Stata output is split across multiple output
--- blocks
-Div = function(el)
-  if el.t == "Div" and
-    el.attr.classes:includes("cell") and
-    #el.content > 0 and
-    el.content[1].t == "CodeBlock" and
-    el.content[1].attr.classes:includes("cell-code") and
-    el.content[1].attr.classes:includes("stata") and
-    -- Don't touch cells with output-location specified
-    el.attr.attributes["output-location"] == nil then
-      -- Check for multiple cell output blocks
-      local hasMultipleOutput = #el.content:filter(
-        function(el)
-          return el.attr.classes:includes("cell-output")
-        end
-      )
-      -- Check for Stata graphic cell output; don't touch these cells
-      local hasStataGraphics = el.content:find_if(
-        function(el)
-          return el.attr.classes:includes("cell-output-display")
-        end
-      )
-      if hasMultipleOutput > 1 and hasStataGraphics == nil then
-        -- Split out the source code and the cell output blocks
-        local codeDiv = el.content[1]
-        local outputDivs = tslice(el.content, 2, #el.content)
-        -- Extract and concatenate text from the output blocks
-        local newOutputText = pandoc.List()
-        for _,output in ipairs(outputDivs) do
-          local outputText = output.content[1].text
-          newOutputText:insert(outputText)
-        end
-        local newOutputText = table.concat(newOutputText)
-        -- Wrap output text in an output block
-        local newOutputDiv = pandoc.Div(
-          pandoc.CodeBlock(newOutputText),
-          pandoc.Attr("", {"cell-output", "cell-output-stdout"})
-        )
-        -- Recompile the source code and output into a new cell
-        local newCell = pandoc.Div(
-          {codeDiv, newOutputDiv},
-          pandoc.Attr(el.attr)
-        )
-        return newCell
-    end
-  end
-end
-
--- Helpers
--- From Quarto source code
+-- Helper from Quarto source code
 -- Quarto license: https://quarto.org/license.html
--- https://github.com/quarto-dev/quarto-cli/blob/9754973987adc13f6722f7a3e279edd16cd66ec2/src/resources/filters/common/table.lua#L18-L25
-function tslice(t, first, last, step)
-  local sliced = {}
-  for i = first or 1, last or #t, step or 1 do
-    sliced[#sliced+1] = t[i]
-  end
-  return sliced
-end
--- https://github.com/quarto-dev/quarto-cli/blob/5b5751dc14b8b7bc5afa9e34135f01a50a953b0c/src/resources/filters/common/pandoc.lua#L20-L22
+-- https://github.com/quarto-dev/quarto-cli/blob/5b5751dc14b8b7bc5afa9e34135f01a50a953b0c/src/resources/filters/common/pandoc.lua
 function removeClass(classes, remove)
   return classes:filter(function(clz) return clz ~= remove end)
 end
